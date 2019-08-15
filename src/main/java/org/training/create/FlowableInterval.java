3710ec41b1f4f6ec381d27e4e84c71d554f4aecb
Alejandro Niño <alejandro6230@gmail.com>
2019-08-16 00:44:30 +0200

    Implement flowable interval

---
+++ src/main/java/org/training/create/FlowableInterval.java
@@ -0,0 +1,26 @@
+package org.training.create;
+
+import io.reactivex.Flowable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.training.utils.Utils.hold;
+
+public class FlowableInterval {
+
+    private static final Logger log = LoggerFactory.getLogger(FlowableCreateSimple.class);
+
+    public static void main(String[] args) {
+
+        Flowable<Long> seconds =
+                Flowable.interval(1, TimeUnit.SECONDS);
+
+        seconds.subscribe(t ->
+                log.info("{}", t));
+
+        hold(2000);
+    }
+
+}
