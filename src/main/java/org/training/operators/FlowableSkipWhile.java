18e44a0fcbf236a1e19352cf7b536ab99e7b92fb
Alejandro Niño <alejandro6230@gmail.com>
2019-08-22 16:35:14 +0200

    Implement flowable skip while operator

---
+++ src/main/java/org/training/operators/FlowableSkipWhile.java
@@ -0,0 +1,21 @@
+package org.training.operators;
+
+import io.reactivex.Flowable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlowableSkipWhile {
+
+    private static final Logger log = LoggerFactory.getLogger(FlowableSkipWhile.class);
+
+    public static void main(String[] args) {
+
+        Flowable<Integer> flowable =
+                Flowable.range(1, 100)
+                        .skipWhile(i -> i <= 95);
+
+        flowable.subscribe(i -> log.info("{}", i));
+
+    }
+
+}
