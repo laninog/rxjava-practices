e60146378ab71f29a6258380be6e7ef0f86be4c0
Alejandro Niño <alejandro6230@gmail.com>
2019-08-31 17:53:17 +0200

    Create a flowable concatenating others

---
+++ src/main/java/org/training/create/FlowableConcat.java
@@ -0,0 +1,25 @@
+package org.training.create;
+
+import io.reactivex.Flowable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlowableConcat {
+
+    private static final Logger log = LoggerFactory.getLogger(FlowableConcat.class);
+
+    public static void main(String[] args) {
+
+        Flowable<String> flowableOne =
+                Flowable.just("Alpha", "Beta", "Gamma", "Delta",
+                        "Epsilon");
+
+        Flowable<String> flowableTwo =
+                Flowable.just("Zeta", "Eta", "Theta");
+
+        Flowable.concat(flowableOne, flowableTwo)
+                .subscribe(w -> log.info("Word {}", w));
+
+    }
+
+}
