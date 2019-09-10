62b1df77d4459283597cb96c5783858e328b5f41
Alejandro Niño <alejandro6230@gmail.com>
2019-09-10 23:48:48 +0200

    Implement flowable zipWith operator

---
+++ src/main/java/org/training/operators/FlowableZipWith.java
@@ -0,0 +1,23 @@
+package org.training.operators;
+
+import io.reactivex.Flowable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlowableZipWith {
+
+    private static final Logger log = LoggerFactory.getLogger(FlowableZipWith.class);
+
+    public static void main(String[] args) {
+
+        Flowable<String> temperature = Flowable.fromArray("10º", "1º", "25º", "2º", "7º");
+
+        Flowable<String> wind = Flowable.fromArray("1km/h", "60km/h", "10km/h", "20km/h", "7km/h");
+
+        Flowable<String> zipWith = temperature.zipWith(wind,
+                (t, w) -> t.concat(" - ").concat(w));
+
+        zipWith.subscribe(log::info);
+    }
+
+}
