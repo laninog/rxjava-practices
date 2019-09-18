daa95d85a2dab6ed1d110fd54c9fea5c66c5e8b1
Alejandro Niño <alejandro6230@gmail.com>
2019-09-19 00:05:01 +0200

    Implement two flowables combine using zip operator

---
+++ src/main/java/org/training/operators/FlowableZipSynchronous.java
@@ -0,0 +1,33 @@
+package org.training.operators;
+
+import io.reactivex.Flowable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.reactivex.Flowable.timer;
+import static org.training.utils.Utils.hold;
+
+public class FlowableZipSynchronous {
+
+    private static final Logger log = LoggerFactory.getLogger(FlowableZipSynchronous.class);
+
+    public static void main(String[] args) {
+
+        Flowable<Long> intervalA =
+                Flowable.interval(1, TimeUnit.SECONDS);
+
+        Flowable<Long> intervalB =
+                Flowable.interval(1, TimeUnit.SECONDS);
+
+        Flowable<Long> zip = Flowable.zip(intervalA.timestamp(), intervalB.timestamp(),
+                (t, w) -> t.time() - w.time());
+
+        zip.subscribe(tt -> log.info("Time between {}", tt));
+
+        hold(6000);
+
+    }
+
+}
