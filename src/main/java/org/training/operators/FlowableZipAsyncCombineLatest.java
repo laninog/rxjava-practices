ad8dd7854dac3249a9faf85ae592caa345c5e08a
Alejandro Niño <alejandro6230@gmail.com>
2019-09-19 00:11:22 +0200

    Implement two flowables handling lack of requests with combineLatest

---
+++ src/main/java/org/training/operators/FlowableZipAsyncCombineLatest.java
@@ -0,0 +1,33 @@
+package org.training.operators;
+
+import io.reactivex.Flowable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.training.utils.Utils.hold;
+
+public class FlowableZipAsyncCombineLatest {
+
+    private static final Logger log = LoggerFactory.getLogger(FlowableZipAsyncCombineLatest.class);
+
+    public static void main(String[] args) {
+
+        Flowable<String> intervalA =
+                Flowable.interval(18, TimeUnit.MILLISECONDS).map(i -> "A".concat(i.toString()));
+
+        Flowable<String> intervalB =
+                Flowable.interval(10, TimeUnit.MILLISECONDS).map(i -> "B".concat(i.toString()));
+
+        Flowable<String> zip = Flowable.combineLatest(intervalA, intervalB,
+                (a, b) -> a.concat(" : ").concat(b));
+
+        zip.subscribe(ab -> log.info("{}", ab),
+                ex -> log.error("Error : {}", ex.getMessage()));
+
+        hold(2000);
+
+    }
+
+}
